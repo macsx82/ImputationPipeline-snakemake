1a252db47b09aefc19a02aca27cf42a476ddc757
macsx82 <macsx82@gmail.com>
2021-06-18 20:24:15 +0200

    added new total report in info stat rule

--- rules/postproc.smk
+++ rules/postproc.smk
@@ -1,40 +1,44 @@
 # rules to generate a report to have a general feel of the imputation quality. By chromosome and by chunk
 rule infoStatsChrom:
 	output:
-		output_folder+"/07.stats/{chr}/{chr}_impute_summary.csv",
+		output_folder+"/07.stats/{chr}/{chr}_impute_summary_by_maf_by_info.csv",
+		output_folder+"/07.stats/{chr}/{chr}_impute_summary_by_maf.csv",
 		output_folder+"/07.stats/{chr}/{chr}_impute_summary.pdf",
 		output_folder+"/07.stats/{chr}/{chr}_impute_manhattan.png"
 	input:
 		output_folder+"/06.imputed/MERGED/{chr}/{chr}.vcf.gz"
 	params:
 		bcftools_bin=config['tools']['bcftools'],
-		scripts_folder=config['paths']['scripts']
+		scripts_folder=config['paths']['scripts'],
+		tab_prefix=output_folder+"/07.stats/{chr}/{chr}_impute_summary"
 	log:
 		stdout=log_folder+"/infoStatsChrom_{chr}.o",
 		stderr=log_folder+"/infoStatsChrom_{chr}.e"
 	shell:
 		"""
-		{params.bcftools_bin} query -f"%CHROM\t%POS\t%ID\t%REF\t%ALT\t%AF\t%INFO/INFO\n" {input} | {params.scripts_folder}/imputationStats.py --tab {output[0]} --fig {output[1]} > {log.stdout} 2> {log.stderr}
+		{params.bcftools_bin} query -f"%CHROM\t%POS\t%ID\t%REF\t%ALT\t%AF\t%INFO/INFO\n" {input} | {params.scripts_folder}/imputationStats.py --tab {params.tab_prefix} --fig {output[1]} > {log.stdout} 2> {log.stderr}
 		{params.bcftools_bin} query -f"%CHROM\t%POS\t%ID\t%REF\t%ALT\t%AF\t%INFO/INFO\n" {input} | {params.scripts_folder}/plot_manhattan.py --cols 0,1,6 --title 'Impute INFO score chr{wildcards.chr}' --image {output[2]} --ymax=5 - >> {log.stdout} 2>> {log.stderr}
 		"""
 
 
 rule infoStatsChunks:
 	output:
-		output_folder+"/07.stats/{chr}/CHUNKS/{chr}_{g_chunk}_impute_summary.csv",
+		output_folder+"/07.stats/{chr}/CHUNKS/{chr}_{g_chunk}_impute_summary_by_maf_by_info.csv",
+		output_folder+"/07.stats/{chr}/CHUNKS/{chr}_{g_chunk}_impute_summary_by_maf.csv",
 		output_folder+"/07.stats/{chr}/CHUNKS/{chr}_{g_chunk}_impute_summary.pdf",
 		output_folder+"/07.stats/{chr}/CHUNKS/{chr}_{g_chunk}_impute_manhattan.png"
 	input:
 		output_folder+"/06.imputed/{chr}/{chr}.{g_chunk}.vcf.gz"
 	params:
 		bcftools_bin=config['tools']['bcftools'],
-		scripts_folder=config['paths']['scripts']
+		scripts_folder=config['paths']['scripts'],
+		tab_prefix=output_folder+"/07.stats/{chr}/CHUNKS/{chr}_{g_chunk}_impute_summary"
 	log:
 		stdout=log_folder+"/infoStatsChunks_{chr}_{g_chunk}.o",
 		stderr=log_folder+"/infoStatsChunks_{chr}_{g_chunk}.e"
 	shell:
 		"""
-		{params.bcftools_bin} query -f"%CHROM\t%POS\t%ID\t%REF\t%ALT\t%AF\t%INFO/INFO\n" {input} | {params.scripts_folder}/imputationStats.py --tab {output[0]} --fig {output[1]} > {log.stdout} 2> {log.stderr}
+		{params.bcftools_bin} query -f"%CHROM\t%POS\t%ID\t%REF\t%ALT\t%AF\t%INFO/INFO\n" {input} | {params.scripts_folder}/imputationStats.py --tab {params.tab_prefix} --fig {output[1]} > {log.stdout} 2> {log.stderr}
 		{params.bcftools_bin} query -f"%CHROM\t%POS\t%ID\t%REF\t%ALT\t%AF\t%INFO/INFO\n" {input} | {params.scripts_folder}/plot_manhattan.py --cols 0,1,6 --title 'Impute INFO score chr{wildcards.chr} chunk {wildcards.g_chunk}' --image {output[2]} --ymax=5 - >> {log.stdout} 2>> {log.stderr}
 		"""
 
--- scripts/imputationStats.py
+++ scripts/imputationStats.py
@@ -29,179 +29,12 @@ def getInfoClass(info):
     elif info >= 0.8:
         return 'gt_08'
 
-# #function to create a manhattan plot, code from Stephane Burgeoise
-# def plotManhattan(dataframe, **params):
-#     # --- Load matplotlib backends ---
-# #
-#     # get dataframe col names for consistency check
-#     header=list(dataframe.columns)
-#     regp=dataframe
-#     # --- working directory and default parameters ---
-#     if 'pvalcolname' in params:
-#         pvalcolname = params['pvalcolname']
-#     else:
-#         pvalcolname = "frequentist_add_pvalue"
-# #
-#     if 'chromcolname' in params:
-#         chromcolname = params['chromcolname']
-#     else:
-#         chromcolname = "chromosome"
-# #
-#     if 'poscolname' in params:
-#         poscolname = params['poscolname']
-#     else:
-#         poscolname = "position"
-# #    
-#     # --- load files ---
-#     print("Loading input file(s)...")
-#     cols = [chromcolname, poscolname, pvalcolname]
-# #   
-#     # --- Set plotting defaults ---
-#     if 'threshold' in params:
-#         threshold = params['threshold']
-#     else:
-#         threshold = 5E-08
-# #
-#     if 'colors' in params:
-#         c = params['colors'].split(',')
-#         if c[0]=="C":
-#             from matplotlib.colors import hex2color
-#             hexc = ["#000080","#306EFF","#F75D59","#990012"]
-#             cs = [hex2color(hexc[i]) for i in range(len(hexc))]
-#             colors = cycle(cs)
-#         else:
-#             colors = cycle(c)
-#     else:
-#         c = ['black', 'grey']
-#         colors = cycle(c)
-# #    
-#     # --- set ymax ---
-#     if 'line' in params:
-#         pmin = min(regp[pvalcolname])
-#         allmin = min([pmin, threshold])
-#         ymax = -np.log10(allmin) + 0.5
-#     else:
-#         pmin = min(regp[pvalcolname])
-#         ymax = -np.log10(pmin) + 0.5
-# #
-#     if 'ymax' in params:
-#         if params['ymax'] < 1:
-#             sys.exit("Invalid Ymax value (i.e. entered value < 1)")
-# #        
-#         if params['ymax'] < ymax:
-#             print("Your selected Ymax value is smaller than the actual Ymax and will be ignored")
-#         else:
-#             ymax = params['ymax']
-# #
-#     print("Ymax is " + str(ymax))
-#     # --- Chromosomes ---
-#     #regp[chromcolname] = regp[chromcolname].astype(str)
-#     if not regp.loc[regp[chromcolname]=='X',:].empty:
-#         regp.loc[regp[chromcolname]=='X',chromcolname] = "23"
-# #
-#     if not regp.loc[regp[chromcolname]=='Y',:].empty:
-#         regp.loc[regp[chromcolname]=='Y',chromcolname] = "24"
-# #
-#     if not regp.loc[regp[chromcolname]=='XY',:].empty:
-#         regp.loc[regp[chromcolname]=='XY',chromcolname] = "25"
-# #
-#     if not regp.loc[regp[chromcolname]=='MT',:].empty:
-#         regp.loc[regp[chromcolname]=='MT',chromcolname] = "26"
-# #
-#     regp[chromcolname] = regp[chromcolname].astype(int)
-# #
-#     allc = natsorted(list(set(regp[chromcolname])))
-#     chromdisplay = []
-#     for x in allc:
-#         if x < 23:
-#             chromdisplay.append(str(x))
-#         else:
-#             if x==23:
-#                 chromdisplay.append("X")
-#             elif x==24:
-#                 chromdisplay.append("Y")
-#             elif x==25:
-#                 chromdisplay.append("XY")
-#             elif x==26:
-#                 chromdisplay.append("MT")
-#             else:
-#                 sys.exit("Abnormal chromosome number found: " + str(x))
-# #                
-#     # --- set x values per chromosome ---
-#     xmax = 0
-#     cs = {}
-#     for chr in allc:
-#         colorc = next(colors)
-#         xstartc = xmax + 10000000
-#         minc = min(regp.loc[regp[chromcolname]==int(chr), poscolname])
-#         maxc = max(regp.loc[regp[chromcolname]==int(chr), poscolname])
-#         sizec = maxc - minc
-#         xmax = xstartc + sizec
-#         xtickc = xmax - (sizec/2)
-#         cs[str(chr)] = [minc, maxc, sizec, xstartc, xtickc, colorc]
-# #
-#     xmax = xmax + 10000000
-#     # --- Open figure and create axes---
-#     # if outformat == ".pdf":
-#     #     pp = PdfPages(outfile)
-#     #     f = plt.figure()
-#     # else:
-#     #     f = plt.figure(figsize=dim, dpi=res)
-#     f = plt.figure()
-#     rect = 0.1,0.1,0.8,0.8
-#     ax = f.add_axes(rect)
-#     ax.set_ylabel('-log10(p-value)')
-#     ax.set_xlabel('chromosome')
-#     plt.ylim(0, ymax)
-#     plt.xlim(0, xmax)
-#     plt.xticks([cs[str(i)][4] for i in allc], chromdisplay, size=4)
-#     ax.get_yaxis().tick_left()
-#     plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='on')
-# #
-#     if 'line' in params:
-#         plt.axhline(y=-math.log10(threshold), color='r', linestyle='--')
-# #
-#     # --- Add title ---
-#     if 'title' in params:
-#         plt.title(params['title'])
-# #
-#     # --- Plot ---
-#     for chr in allc:
-#         print("Plotting chromosome " + str(chr))
-#         xs = np.array(regp.loc[(regp[chromcolname]==int(chr)) & (regp[pvalcolname]>threshold), poscolname]) - cs[str(chr)][0] + cs[str(chr)][3]
-#         ys = -np.log10(np.array(regp.loc[(regp[chromcolname]==int(chr)) & (regp[pvalcolname]>threshold), pvalcolname]))
-#         ax.plot(np.squeeze(xs), np.squeeze(ys), linestyle='', marker='o', color=cs[str(chr)][5], markerfacecolor=cs[str(chr)][5], markersize=2, markeredgecolor=cs[str(chr)][5], alpha=0.5)
-#         # Plot hits below threshold in green
-#         sig = regp.loc[(regp[chromcolname]==int(chr)) & (regp[pvalcolname]<=threshold), poscolname]
-#         if len(sig) > 0:
-#             xs2 = np.array(sig) - cs[str(chr)][0] + cs[str(chr)][3]
-#             ys2 = -np.log10(np.array(regp.loc[(regp[chromcolname]==int(chr)) & (regp[pvalcolname]<=threshold), pvalcolname]))
-#             ax.plot(np.squeeze(xs2), np.squeeze(ys2), linestyle='', marker='^', color='green', markerfacecolor='green', markersize=3, markeredgecolor='green', alpha=0.5)
-
-    # # --- Save file ---
-    # print("Done! Now saving figure...")
-    # if outformat == ".pdf":
-    #     pp.savefig()
-    #     pp.close()
-    #     plt.close()
-    # elif outformat == ".eps":
-    #     plt.savefig(outfile, dpi=res, format='eps', pad_inches=0.1)
-    #     plt.close()
-    # elif outformat == ".svg":
-    #     plt.savefig(outfile, dpi=res, format='svg', pad_inches=0.1)
-    #     plt.close()
-    # else:
-    #     plt.savefig(outfile, dpi=res, format='png', pad_inches=0.1)
-    #     plt.close()
-
-# print("Successfully finished. The run took: ",(datetime.now()-startTime))
-
 ################################################################################
 
 
 #we will read from stdin, but we want to specify file names for table and pdf/figure output
 parser = argparse.ArgumentParser()
-parser.add_argument('--tab', type=str, help="MANDATORY: Provide full path to the output file for the table report.")
+parser.add_argument('--tab', type=str, help="MANDATORY: Provide full path to the output file for the table report, but only the name prefix.")
 parser.add_argument('--fig', type=str, help="MANDATORY: Provide full path to the output file for the plot (PDF) report.")
 
 args=parser.parse_args()
@@ -227,6 +60,7 @@ chrom_table['MAF'] = [1 - x if x > 0.5 else x for x in chrom_table.AF ]
 descr_stats = chrom_table[['INFO_SCORE','MAF']].describe()
 
 # get all sites with INFO score eq 0
+tot_sites = len(chrom_table)
 info_zero = len(chrom_table[chrom_table.INFO_SCORE == 0])
 
 # get all sites with INFO score lt 0.4 and gt 0
@@ -244,11 +78,29 @@ chrom_table['INFO_CLASS'] = chrom_table.INFO_SCORE.apply(getInfoClass)
 # extract counts for info score stratified by maf classes and info score classes
 resume_by_info_maf_classes=chrom_table[['INFO_SCORE','MAF_CLASS','INFO_CLASS']].groupby(['INFO_CLASS','MAF_CLASS']).describe()
 # write the table
-resume_by_info_maf_classes.to_csv(args.tab)
+resume_by_info_maf_classes.to_csv(args.tab + '_by_maf_by_info.csv')
 # out_tab.close()
-# we also want a manhattan-like plot
-# chrom_table['INFO_LOGP'] = (numpy.log10(chrom_table.INFO_SCORE+1))
-# chrom_table['INFO_LOGP'] = -(numpy.log10(chrom_table.INFO_SCORE))
+
+# I want a table with:
+# tot site number | tot sites info >=0.4 | ratio of good sites
+# overall and by maf class
+tot_sites_common = len(chrom_table[(chrom_table.MAF_CLASS == 'common')])
+tot_sites_low = len(chrom_table[(chrom_table.MAF_CLASS == 'low')])
+tot_sites_rare = len(chrom_table[(chrom_table.MAF_CLASS == 'rare')])
+tot_sites_by_classes = [tot_sites, tot_sites_common,tot_sites_low,tot_sites_rare]
+
+info_04_sites_common = len(chrom_table[(chrom_table.MAF_CLASS == 'common') & ((chrom_table.INFO_CLASS == 'gt_04_lt_08') | (chrom_table.INFO_CLASS == 'gt_08')) ])
+info_04_sites_low = len(chrom_table[(chrom_table.MAF_CLASS == 'low') & ((chrom_table.INFO_CLASS == 'gt_04_lt_08') | (chrom_table.INFO_CLASS == 'gt_08')) ])
+info_04_sites_rare = len(chrom_table[(chrom_table.MAF_CLASS == 'rare') & ((chrom_table.INFO_CLASS == 'gt_04_lt_08') | (chrom_table.INFO_CLASS == 'gt_08')) ])
+good_sites_by_classes = [info_04,info_04_sites_common,info_04_sites_low, info_04_sites_rare]
+
+
+resume = pd.DataFrame(list(zip(tot_sites_by_classes,good_sites_by_classes)), columns=['tot_sites','good_sites'],index=['tot','common','low','rare'])
+
+resume['good_prop'] = (resume.good_sites/resume.tot_sites)*100
+
+resume.to_csv(args.tab + '_by_maf.csv')
+
 
 # we also want to generate some density plots
 # We want to plot the distribution for each MAF slice
@@ -265,15 +117,3 @@ plt.legend(loc='best')
 # plt.savefig('TEST4.pdf')
 plt.savefig(args.fig)
 plt.close()
-# plt.close(fig)
-
-
-# params={}
-# params['chromcolname'] = 'CHROM'
-# params['poscolname'] = 'POS'
-# params['pvalcolname'] = 'INFO_SCORE'
-# # params['mafcolname'] = 'MAF'
-# # params['infocolname'] = 'INFO_SCORE'
-# params['title'] = 'Info score Manatthan plot'
-# params['threshold'] = 0.01
-# params['res'] = 300
